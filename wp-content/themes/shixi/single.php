a18b286eccd4672614eaba4c1c11842b31d1bfbc
Uice Lu <uicestone@gmail.com>
2018-01-04 14:02:52 +0800

    更新banner

--- wp-content/themes/shixi/single.php
+++ wp-content/themes/shixi/single.php
@@ -4,7 +4,7 @@
 	<tbody>
 	<tr>
 		<td>
-			<img src="<?=get_stylesheet_directory_uri()?>/images/banner.jpg?ver=2018-01-01"
+			<img src="<?=get_stylesheet_directory_uri()?>/images/banner1.jpg?ver=2018-01-01"
 				 name="slide" border="0" width="998" height="234" style="object-fit: cover; float: left;
                     z-index: 1; padding: 0px; margin: 0px; filter: progid:DXImageTransform.Microsoft.Pixelate(MaxSquare=15,Duration=1)"
 				 onclick="alert(this.src)">
