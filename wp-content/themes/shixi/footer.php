13e00e9e7f92c16aa7b69a0e6128c7ed98197b71
Uice Lu <uicestone@gmail.com>
2018-01-07 21:38:07 +0800

    底部备案号和样式

--- wp-content/themes/shixi/footer.php
+++ wp-content/themes/shixi/footer.php
@@ -1,7 +1,9 @@
+                <hr style="border: 1px solid #b4e4ec;width:998px;margin:1em 0">
                 <table width="998" border="0" cellspacing="0" cellpadding="0">
                     <tr>
                         <td height="36">
-                            <div align="center" class="STYLE5">上海市新民实验学校 2017 版权所有 地址:上海市宝山区月浦四村1号</div>
+                            <div align="center" class="STYLE5">上海市宝山区新民实验学校 &copy;2017  地址:上海市宝山区月浦四村1号 联系电话：021-56649303</div>
+                            <div align="center" class="STYLE5">沪ICP备17010321号-1</div>
                         </td>
                     </tr>
                 </table>
